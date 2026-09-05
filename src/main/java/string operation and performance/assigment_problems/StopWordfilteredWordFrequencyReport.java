fb68b907332bda43b1f10d5e1304852658f3cd3d
itsdsk2020 <itsdsk2020@gmail.com>
2026-09-05 11:21:10 +0530

    Add Session 2 assignment problems

---
+++ src/main/java/string operation and performance/assigment_problems/StopWordfilteredWordFrequencyReport.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class StopWordFilteredWordFrequencyReport {
+
+    static void printFilteredWordFrequency(String feedback) {
+
+        String cleaned = feedback.toLowerCase()
+                .replace(".", "")
+                .replace(",", "");
+
+        String[] words = cleaned.split("\\s+");
+
+        String[] stopWords = {"the", "was", "and", "a", "is", "of", "in"};
+
+        HashMap<String, Integer> frequency = new HashMap<>();
+
+        for (String word : words) {
+
+            boolean isStopWord = false;
+
+            for (String stopWord : stopWords) {
+
+                if (word.equals(stopWord)) {
+                    isStopWord = true;
+                    break;
+                }
+            }
+
+            if (!isStopWord) {
+                frequency.put(word, frequency.getOrDefault(word, 0) + 1);
+            }
+        }
+
+        ArrayList<Map.Entry<String, Integer>> list =
+                new ArrayList<>(frequency.entrySet());
+
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        for (Map.Entry<String, Integer> entry : list) {
+
+            System.out.println(
+                entry.getKey() + ": " + entry.getValue()
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter feedback: ");
+        String feedback = sc.nextLine();
+
+        printFilteredWordFrequency(feedback);
+
+        sc.close();
+    }
+}
